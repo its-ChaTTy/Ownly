a63e25dbc4283c7b230f395409f91a36c7788e4f
Sujith <ps335@snu.edu.in>
2023-11-22 22:40:49 +0530

    request servicess

---
+++ services/requests.service.js
@@ -0,0 +1,3 @@
+import db from '@/lib/prisma'
+
+
--- services/user.service.js
+++ services/user.service.js
@@ -22,3 +22,71 @@ export async function updateUser(id, data) {
         data,
     });
 }
+
+export async function deleteUser(id) {
+    return db.user.delete({
+        where: {
+            id,
+        },
+    });
+}
+
+export async function fetchUserById(id) {
+    return db.user.findUnique({
+        where: {
+            id,
+        },
+        include: {
+            items: true,
+            RentRequest: true,
+            ActiveRent: true,
+            OverRent: true,
+        },
+    });
+}
+
+
+export async function getUserItems(id) {
+    return db.user.findUnique({
+        where: {
+            id,
+        },
+        include: {
+            items: true,
+        },
+    });
+}
+
+export async function getUserRentRequests(id) {
+    return db.user.findUnique({
+        where: {
+            id,
+        },
+        include: {
+            RentRequest: true,
+        },
+    });
+}
+
+export async function getUserActiveRents(id) {
+    return db.user.findUnique({
+        where: {
+            id,
+        },
+        include: {
+            ActiveRent: true,
+        },
+    });
+}
+
+export async function getUserRentHistory(id) {
+    return db.user.findUnique({
+        where: {
+            id,
+        },
+        include: {
+            OverRent: true,
+        },
+    });
+}
+
