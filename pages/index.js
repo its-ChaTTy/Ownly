464928df14f6d33ae26f5d5f3e439925c7e210be
Sujith <ps335@snu.edu.in>
2023-11-24 20:37:02 +0530

    uncomment and seperate comps

--- pages/index.js
+++ pages/index.js
@@ -1,13 +1,18 @@
 import Footer from "@/components/Footer/Footer";
 import Navbar from "@/components/Navbar/Navbar";
 import HeroBrowse from "@/components/HeroBrowse/HeroBrowse";
+
 export default function Home() {
   return (
     <>
       <div className="section">
         <Navbar />
-        {/* <HeroBrowse /> */}
-        {/* <Footer /> */}
+      </div>
+      <div className="section">
+        <HeroBrowse />
+      </div>
+      <div className="section">
+        <Footer />
       </div>
     </>
   );
