fa36d69520811ec97676f9891fd3f8002f1126c8
Sujith <ps335@snu.edu.in>
2023-11-26 02:46:35 +0530

    add listItem comp

---
+++ components/ListItem/ListItem.js
@@ -0,0 +1,19 @@
+import './ListItem.scss'
+import { 
+    Card,
+    Select,
+    Button,
+    FormControl,
+    FormLabel,
+    Input,
+    Textarea,
+     
+} from '@chakra-ui/react'
+
+function ListItem() {
+    return (
+        <div>ListItem</div>
+    )
+}
+
+export default ListItem
