62e95441be2ed9b4c1b04be69c5957d78270e996
Ary0405 <aryansethia2003@gmail.com>
2023-11-29 23:06:53 +0530

    feat: hero, navbar and about desktop view frontend added

--- components/Navbar/Navbar.js
+++ components/Navbar/Navbar.js
@@ -7,81 +7,78 @@ import "./Navbar.scss";
 import { useRouter } from "next/router";
 
 function Navbar() {
-  const router = useRouter();
-  const [isSidebarOpen, setIsSidebarOpen] = useState(false);
+	const router = useRouter();
+	const [isSidebarOpen, setIsSidebarOpen] = useState(false);
 
-  const handleLogin = () => {
-    router.push("/auth/login");
-  };
+	const handleLogin = () => {
+		router.push("/auth/login");
+	};
 
-  const handleSignup = () => {
-    router.push("/auth/register");
-  };
+	const handleSignup = () => {
+		router.push("/auth/register");
+	};
 
-  return (
-    <div className="Navbar">
-      <div className="Navbar__container">
-        <FaBars className="fabar-icon" onClick={() => setIsSidebarOpen(true)} />
-        <div className="Navbar__leftside">
-          <div className="Navbar__container--logo">
-            <Image
-              src="/Images/Logos/ownly_footer_logo.png"
-              alt="Your Logo"
-              width={100}
-              height={100}
-            />
-          </div>
-          <div className="Navbar__container--pages">
-            <p>Home</p>
-            <p>How it works</p>
-            <p>About us</p>
-            <p>Contact</p>
-          </div>
-        </div>
-        <div className="Navbar__rightside">
-          <div className="Navbar__container--search">
-            <input
-              type="text"
-              className="search-bar"
-              placeholder="Search for items to rent"
-            />
-          </div>
-          <div>
-            <button className="signup-button" onClick={handleSignup}>
-              Sign Up
-            </button>
-          </div>
-          <div>
-            <button className="login-button" onClick={handleLogin}>
-              Login
-            </button>
-          </div>
-        </div>
-      </div>
-      {isSidebarOpen && (
-        <div className="Navbar__sidebar">
-          <FaTimes
-            className="close-icon"
-            onClick={() => setIsSidebarOpen(false)}
-          />
-          <div className="Navbar__container--logo">
-            <Image
-              src="/Images/Logos/ownly_footer_logo.png"
-              alt="Your Logo"
-              width={100}
-              height={100}
-            />
-          </div>
-          <div className="Navbar__container--pages">
-            <p>Home</p>
-            <p>How it works</p>
-            <p>About us</p>
-            <p>Contact</p>
-          </div>
-        </div>
-      )}
-    </div>
-  );
+	return (
+		<div className="Navbar">
+			<div className="Navbar__container">
+				<FaBars className="fabar-icon" onClick={() => setIsSidebarOpen(true)} />
+				<div className="Navbar__leftside">
+					<div className="Navbar__container--logo">
+						<Image
+							src="/Images/Logos/ownly_footer_logo.png"
+							alt="Your Logo"
+							width={100}
+							height={100}
+						/>
+					</div>
+					<div className="Navbar__container--pages">
+						<p>Home</p>
+						<p>How it works</p>
+						<p>About us</p>
+						<p>Contact</p>
+					</div>
+				</div>
+				<div className="Navbar__rightside">
+					<div className="Navbar__container--search">
+						<input
+							type="text"
+							className="search-bar"
+							placeholder="Search for items to rent"
+						/>
+					</div>
+					<div>
+						<Image style={{cursor: "pointer"}} src={'/Images/Assets/profile.png'} width={43} height={43} />
+					</div>
+					<div>
+						<Image style={{cursor: "pointer"}} src={'/Images/Assets/cart.png'} width={43} height={43} />
+
+					</div>
+				</div>
+			</div>
+			{isSidebarOpen && (
+				<div className="Navbar__sidebar">
+					<FaTimes
+						className="close-icon"
+						onClick={() => setIsSidebarOpen(false)}
+					/>
+					<div className="Navbar__container--logo">
+						<Image
+							src="/Images/Logos/ownly_footer_logo.png"
+							alt="Your Logo"
+							width={100}
+							height={100}
+						/>
+					</div>
+					<div className="Navbar__container--pages">
+						<p>Home</p>
+						<p>How it works</p>
+						<p>About us</p>
+						<p>Contact</p>
+					</div>
+				</div>
+			)}
+		</div>
+	);
 }
 
 export default Navbar;
--- pages/index.js
+++ pages/index.js
@@ -1,15 +1,19 @@
 import Footer from "@/components/Footer/Footer";
 import Navbar from "@/components/Navbar/Navbar";
 import HeroBrowse from "@/components/HeroBrowse/HeroBrowse";
-import ListItem from "@/components/ListItem/ListItem";
+import '../styles/routes/index.scss'
+import About from "@/components/About/About";
 export default function Home() {
   return (
     <>
-      <div className="section">
+      <div className="section_navbar">
         <Navbar />
       </div>
       <div className="section">
         <HeroBrowse />
+      </div>
+      <div className="section">
+        <About />
       </div>
       <div className="section">
         <Footer />
